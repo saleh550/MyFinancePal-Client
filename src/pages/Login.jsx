618f1a7cf43c6f28d01447c1c83e060e051615ea
סאלח פארס <75793688+saleh550@users.noreply.github.com>
2024-01-27 02:08:38 +0200

    login and register form completed

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,74 @@
-function Login(){
-    return(<>
-        Login
-    </>)
+import { useTranslation } from "react-i18next";
+import { useState } from "react";
+
+function Login() {
+  const { t } = useTranslation();
+  const [formData, setFormData] = useState({
+    userName: "",
+    password: "",
+  });
+  const { userName, password } = formData;
+  const onChange = (e) => {
+    e.preventDefault();
+    setFormData((prev) => {
+      return {
+        ...prev,
+        [e.target.name]: e.target.value,
+      };
+    });
+  };
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData)
+  };
+  return (
+    <>
+      <div className="container mt-5">
+        <h1 className="my-3">{t("LOGINING")}</h1>
+        <form onSubmit={onSubmit} className="row g-4">
+          <div className="col-md-6">
+            <label for="validationDefaultUsername" class="form-label">
+              {t("USERNAME")}
+            </label>
+            <div dir="ltr" class="input-group">
+              <span class="input-group-text" id="inputGroupPrepend2">
+                @
+              </span>
+              <input
+                onChange={onChange}
+                type="text"
+                className="form-control"
+                name="userName"
+                value={userName}
+                id="validationDefaultUsername"
+                aria-describedby="inputGroupPrepend2"
+                required
+              />
+            </div>
+          </div>
+          <div class="col-md-5">
+            <label for="validationDefault02" class="form-label">
+              {t("PASSWORD")}
+            </label>
+            <input
+              name="password"
+              type="password"
+              onChange={onChange}
+              class="form-control"
+              id="validationDefault02"
+              value={password}
+              required
+            />
+          </div>
+
+          <div className="col-12">
+            <button className="btn btn-success" type="submit">
+              {t("LOGIN")}
+            </button>
+          </div>
+        </form>
+      </div>
+    </>
+  );
 }
-export default Login
+export default Login;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,6 +1,105 @@
-function Register(){
-    return(<>
-        Register
-    </>)
+import { useState } from "react";
+import { useTranslation } from "react-i18next";
+
+function Register() {
+  const { t } = useTranslation();
+  const [ formData, setFormData ] = useState({
+    fullName: "",
+    userName: "",
+    password: "",
+    comfortPassword: "",
+  });
+  const { fullName, userName, password, comfortPassword } = formData;
+  const onChange = (e) => {
+    e.preventDefault();
+    setFormData((prev) => {
+      return {
+        ...prev,
+        [e.target.name]: e.target.value,
+      };
+    });
+
+  };
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
+  return (
+    <>
+      <div className="container mt-5">
+        <h1 className="my-3">{t("REGISTER")}</h1>
+        <form onSubmit={onSubmit} class="row g-4">
+          <div class="col-md-4">
+            <label for="validationDefault01" class="form-label">
+              {t("FULLNAME")}
+            </label>
+            <input
+              onChange={onChange}
+              type="text"
+              class="form-control"
+              id="validationDefault01"
+              name="fullName"
+              value={fullName}
+              required
+            />
+          </div>
+
+          <div class="col-md-6">
+            <label for="validationDefaultUsername" class="form-label">
+              {t("USERNAME")}
+            </label>
+            <div dir="ltr" class="input-group">
+              <span class="input-group-text" id="inputGroupPrepend2">
+                @
+              </span>
+              <input
+                onChange={onChange}
+                type="text"
+                class="form-control"
+                id="validationDefaultUsername"
+                aria-describedby="inputGroupPrepend2"
+                name="userName"
+                value={userName}
+                required
+              />
+            </div>
+          </div>
+          <div class="col-md-5">
+            <label for="validationDefault02" class="form-label">
+              {t("PASSWORD")}
+            </label>
+            <input
+              onChange={onChange}
+              type="password"
+              class="form-control"
+              id="validationDefault02"
+              name="password"
+              value={password}
+              required
+            />
+          </div>
+          <div class="col-md-5">
+            <label for="validationDefault02" class="form-label">
+              {t("COMFORT_PASSWORD")}
+            </label>
+            <input
+              onChange={onChange}
+              type="password"
+              class="form-control"
+              id="validationDefault02"
+              name="comfortPassword"
+              value={comfortPassword}
+              required
+            />
+          </div>
+          <div class="col-12">
+            <button class="btn btn-success" type="submit">
+              {t("REGISTER")}
+            </button>
+          </div>
+        </form>
+      </div>
+    </>
+  );
 }
-export default Register
+export default Register;
